e398c8b0e4c8b157f980b9af8ecc131759ab3584
Cristhian Figueroa <cfigmart@users.noreply.github.com>
2022-10-06 15:18:57 -0500

    Agrego una nueva rama

--- app/src/main/java/edu/unicauca/aplimovil/principal/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/principal/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //Este es el método por donde inicia la aplicación
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
